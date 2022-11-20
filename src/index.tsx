22503579df0b394a0627d2c2cc94d60fc344a89e
julgrabar <45499113+julgrabar@users.noreply.github.com>
2022-11-20 13:56:46 +0200

    rename basename

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <BrowserRouter basename="/test-task/">
+  <BrowserRouter basename="/test-task-api/">
     <React.StrictMode>
       <App />
     </React.StrictMode>
